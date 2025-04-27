de5b8df0e915885dd0f6296b93b7d791998f1d83
rofusedcryof <dezttroll@gmail.com>
2025-04-27 18:44:09 +0700

    menghapus class nyasar

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const taskInput = document.querySelector("input[name='task']");
-    taskInput.addEventListener("input", () => {
-        taskInput.style.background = taskInput.value ? "#e3f2fd" : "white";
-    });
-});
